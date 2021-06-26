c621228cfb7817337820552d956722c424db7795
RITAYAN PATRA <51791113+RITS98@users.noreply.github.com>
2021-06-26 17:09:10 +0530

    st

--- Dynamic Programming/printLCS.java
+++ Dynamic Programming/printLCS.java
@@ -8,9 +8,9 @@ class printLCS
 		for(int j = 0;j<=m;j++)
 			dp[0][j] = 0;
 
-		for(int i = 0;i<=n;i++)
+		for(int i = 1;i<=n;i++)
 		{
-			for(int j = 0;j<=m;j++)
+			for(int j = 1;j<=m;j++)
 			{
 				if(s1.charAt(i-1) == s2.charAt(j-1))
 					dp[i][j] = dp[i-1][j-1] + 1;
@@ -29,7 +29,7 @@ class printLCS
 			{
 				sb.insert(0, s1.charAt(i-1));
 				i--;
-				j--''
+				j--;
 			}
 			else if(dp[i-1][j]>dp[i][j-1])
 				i--;
